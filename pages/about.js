49ee62a795dec87b9e74f2c7630ade32e2f909ca
Tai Zeng <zengtai@gmail.com>
2023-03-18 14:37:32 +0800

    修改关于页

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,29 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import { SITE_META } from "../lib/constants";
-import { FEATURED_GAMES } from "../lib/constants";
 
 export default function About(params) {
-  const toTitle = (name) =>
-    name
-      .replace(/([A-Z])/g, " $1")
-      .trim()
-      .replace(/3 D/g, " 3D")
-      .replace(/([A-Za-z])([0-9])/g, "$1 $2");
+  const content = `
+    <p>
+      Welcome to our ultimate online gaming platform - your go-to destination for a wide
+      variety of free online games. At PlayGamesFree.xyz, we offer everything from thrilling
+      action games to brain-teasing puzzles and exciting sports games.
+    </p>
+    <p>
+      Our user-friendly interface makes it easy to browse and find games that suit your
+      interests, and you can play them all directly in your browser - no downloads
+      necessary!
+    </p>
+    <p>
+      We take pride in offering high-quality games that are sure to keep you entertained for
+      hours on end. With our free, fun games, you can enjoy gaming anytime, anywhere.
+    </p>
+    <p>
+      So, whether you're looking for a quick gaming fix during your lunch break or a full
+      day of non-stop entertainment, PlayGamesFree.xyz has got you covered. Come join the
+      fun and start playing today!"
+    </p>
+  `;
   return (
     <Layout>
       <Head>
@@ -20,27 +34,10 @@ export default function About(params) {
           <div className="section-head">
             <h2 className="h2">About</h2>
           </div>
-          <div className="mx-8 p-4 rounded-md text-yellow-900 mb-4 bg-amber-200">
-            <p>
-              Welcome to our ultimate online gaming platform - your go-to destination for a wide
-              variety of free online games. At PlayGamesFree.xyz, we offer everything from thrilling
-              action games to brain-teasing puzzles and exciting sports games.
-            </p>
-            <p>
-              Our user-friendly interface makes it easy to browse and find games that suit your
-              interests, and you can play them all directly in your browser - no downloads
-              necessary!
-            </p>
-            <p>
-              We take pride in offering high-quality games that are sure to keep you entertained for
-              hours on end. With our free, fun games, you can enjoy gaming anytime, anywhere.
-            </p>
-            <p>
-              So, whether you're looking for a quick gaming fix during your lunch break or a full
-              day of non-stop entertainment, PlayGamesFree.xyz has got you covered. Come join the
-              fun and start playing today!"
-            </p>
-          </div>
+          <div
+            className="mx-8 p-4 rounded-md text-yellow-900 mb-4 bg-amber-200"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
         </section>
       </div>
     </Layout>
